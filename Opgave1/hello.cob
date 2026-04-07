817ec28e2876ed6e0eb771260060f2021741db8b
Alexander A <alexaldilemi05@gmail.com>
2026-04-07 14:50:27 +0200

    Initial commit

---
+++ Opgave1/hello.cob
@@ -0,0 +1,11 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 VAR-TEXT                 PIC X(30) VALUE "HELLO med Variabel".
+
+       PROCEDURE DIVISION.
+      *Nedenfor kommer en display- Cobols måde at skrive i konsollen
+       DISPLAY VAR-TEXT
+       STOP RUN.
