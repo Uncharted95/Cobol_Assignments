8cb02be8749aea543ff2de82b596860d888ed10a
Alexander A <alexaldilemi05@gmail.com>
2026-04-08 12:12:10 +0200

    Task 2
    Completed Task 2 which involving moving variables

---
+++ Task2/Opgave2.cob
@@ -0,0 +1,26 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Opgave2.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01  Kunde-id                PIC X(10).
+       01  Fornavn                 PIC X(20).
+       01  Efternavn               PIC X(20).
+       01  Kontonummer             PIC X(20).
+       01  Valutakode              PIC X(3).
+
+
+       PROCEDURE DIVISION.
+      *Nedenfor kommer en display- Cobols måde at skrive i konsollen
+       MOVE "123456" TO Kunde-id
+       MOVE "Alexander" TO Fornavn
+       MOVE "Jensen" TO Efternavn
+       MOVE "DK1234567" TO Kontonummer
+       MOVE "DKK" TO Valutakode
+
+       DISPLAY Kunde-id
+       DISPLAY Fornavn
+       DISPLAY Efternavn
+       DISPLAY Kontonummer
+       DISPLAY Valutakode
+       STOP RUN.
