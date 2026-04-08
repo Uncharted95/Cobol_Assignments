1ef154afa4c68c5ed989fbc2cd8bc6f861836327
Alexander A <alexaldilemi05@gmail.com>
2026-04-08 13:17:02 +0200

    Added task3
    Updated the code and added the string command to mix variables into a new empty one.

--- Task2/Opgave2.cob
+++ Task2/Opgave2.cob
@@ -3,17 +3,17 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01  Kunde-id                PIC X(10).
-       01  Fornavn                 PIC X(20).
-       01  Efternavn               PIC X(20).
-       01  Kontonummer             PIC X(20).
-       01  Valutakode              PIC X(3).
+       01  Kunde-id                PIC X(10) VALUE SPACES.
+       01  Fornavn                 PIC X(20) VALUE SPACES.
+       01  Efternavn               PIC X(20) VALUE SPACES.
+       01  Kontonummer             PIC X(20) VALUE SPACES.
+       01  Valutakode              PIC X(3) VALUE SPACES.
 
 
        PROCEDURE DIVISION.
       *Nedenfor kommer en display- Cobols måde at skrive i konsollen
        MOVE "123456" TO Kunde-id
-       MOVE "Alexander" TO Fornavn
+       MOVE "Aleksander" TO Fornavn
        MOVE "Jensen" TO Efternavn
        MOVE "DK1234567" TO Kontonummer
        MOVE "DKK" TO Valutakode
---
+++ Task3/Opgave3.cob
@@ -0,0 +1,23 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Opgave3.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01  Kunde-id                PIC X(10) VALUE SPACES.
+       01  Fornavn                 PIC X(20) VALUE SPACES.
+       01  Efternavn               PIC X(20) VALUE SPACES.
+       01  Navn                    PIC X(40) VALUE SPACES.
+       01  Kontonummer             PIC X(20) VALUE SPACES.
+       01  Valutakode              PIC X(3) VALUE SPACES. 
+
+       PROCEDURE DIVISION.
+       MOVE "Alexander" TO Fornavn
+       MOVE "Jensen" TO Efternavn
+
+       STRING Fornavn DELIMITED BY SIZE " "
+              DELIMITED BY SIZE Efternavn
+              DELIMITED BY SIZE
+              INTO Navn
+      *Nedenfor kommer en display- Cobols måde at skrive i konsollen
+       DISPLAY Navn
+       STOP RUN.
