6a2674a11631e4a98ce5386f3dcaf800117cfd26
Alexander A <alexaldilemi05@gmail.com>
2026-04-15 10:20:52 +0200

    Structures
    Added structures to the variables from the previous assignments

--- Task4/Opgave4.cob
+++ Task4/Opgave4.cob
@@ -2,19 +2,25 @@
        PROGRAM-ID. Opgave3.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01  Kunde-id                PIC X(10) VALUE SPACES.
-       01  Fornavn                 PIC X(20) VALUE SPACES.
-       01  Efternavn               PIC X(20) VALUE SPACES.
-       01  Navn                    PIC X(40) VALUE SPACES.
-       01  Kontonummer             PIC X(20) VALUE SPACES.
-       01  Valutakode              PIC X(3)  VALUE SPACES.
+
+       01 KUNDEOPL                 PIC X(10) VALUE SPACES.
+           02  Kunde-id                PIC X(10) VALUE SPACES.
+           02  Fornavn                 PIC X(20) VALUE SPACES.
+           02  Efternavn               PIC X(20) VALUE SPACES.
+           02  Navn                    PIC X(40) VALUE SPACES.
+       
+       01  KONTOINFO               PIC X(10) VALUE SPACES.
+           02  Kontonummer             PIC X(20) VALUE SPACES.
+           02  Valutakode              PIC X(3)  VALUE SPACES.
+
+           02  Balance-linje           PIC X(25) VALUE SPACES.
        01  Renset-navn             PIC X(40) VALUE SPACES.
        01  IX                      PIC 9(2)  VALUE 1.
        01  IX2                     PIC 9(2)  VALUE 1.
        01  Current-Char            PIC X(1)  VALUE SPACES.
        01  Previous-Char           PIC X(1)  VALUE SPACES.
        01  Balance                 PIC X(20) VALUE SPACES.
-       01  Balance-linje           PIC X(25) VALUE SPACES.
+     
        
 
        PROCEDURE DIVISION.
@@ -50,10 +56,7 @@
        END-PERFORM
        
       *Nedenfor kommer en display- Cobols måde at skrive i konsollen
-       DISPLAY Kunde-id
-       DISPLAY Renset-Navn
-       DISPLAY Kontonummer
-       DISPLAY Balance-linje
-       
+       DISPLAY KUNDEOPL
+       DISPLAY KONTOINFO
 
        STOP RUN.
