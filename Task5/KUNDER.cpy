df005c01de155c983349d0154c31f400be5f1cba
Alexander A <alexaldilemi05@gmail.com>
2026-04-15 14:23:21 +0200

    Copybook usage

--- Task5/KUNDER.cpy
+++ Task5/KUNDER.cpy
@@ -1,21 +1,24 @@
+       01 KUNDE-POST.
            02  Kunde-id                PIC X(10) VALUE SPACES.
            02  Fornavn                 PIC X(20) VALUE SPACES.
            02  Efternavn               PIC X(20) VALUE SPACES.
            02  Navn                    PIC X(40) VALUE SPACES.
-           02  Kontonummer             PIC X(20) VALUE SPACES.
-           02  Valutakode              PIC X(3)  VALUE SPACES.
-           02  Balance-linje           PIC X(25) VALUE SPACES.
 
-       02 ADDRESSE.
-           03 VEJNAVN                  PIC X(30).
-           03 HUSNR                    PIC X(5).
-           03 ETAGE                    PIC X(5).
-           03 SIDE                     PIC X(5).
-           03 BY                       PIC X(20).
-           03 POSTNR                   PIC X(4).
-           03 LANDE-KODE               PIC X(2).
+           02  ADDRESSE.
+               03  VEJNAVN             PIC X(30).
+               03  HUSNR               PIC X(5).
+               03  ETAGE               PIC X(5).
+               03  SIDE                PIC X(5).
+               03  BYNAVN              PIC X(20).
+               03  POSTNR              PIC X(4).
+               03  LANDE-KODE          PIC X(2).
 
-       02 
-           03 TELEFON                  PIC X(8).
-           03 EMAIL                    PIC X(50).
- 
+           02  KONTAKT-INFO.
+               03  TELEFON             PIC X(8).
+               03  EMAIL               PIC X(50).
+
+           02  KONTOINFO.
+               03  Kontonummer         PIC X(20) VALUE SPACES.
+               03  Valutakode          PIC X(3)  VALUE SPACES.
+               03  Balance             PIC X(20) VALUE SPACES.
+               03  Balance-linje       PIC X(25) VALUE SPACES.
--- Task5/Opgave5.cob
+++ Task5/Opgave5.cob
@@ -1,28 +1,15 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. Opgave3.
+       PROGRAM-ID. Opgave5.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
 
-       01 KUNDEOPL                 PIC X(10) VALUE SPACES.
-           COPY "KUNDER.cpy"
-           02  Kunde-id                PIC X(10) VALUE SPACES.
-           02  Fornavn                 PIC X(20) VALUE SPACES.
-           02  Efternavn               PIC X(20) VALUE SPACES.
-           02  Navn                    PIC X(40) VALUE SPACES.
-       
-       01  KONTOINFO               PIC X(10) VALUE SPACES.
-           02  Kontonummer             PIC X(20) VALUE SPACES.
-           02  Valutakode              PIC X(3)  VALUE SPACES.
+       COPY "KUNDER.cpy".
 
-           02  Balance-linje           PIC X(25) VALUE SPACES.
        01  Renset-navn             PIC X(40) VALUE SPACES.
        01  IX                      PIC 9(2)  VALUE 1.
        01  IX2                     PIC 9(2)  VALUE 1.
        01  Current-Char            PIC X(1)  VALUE SPACES.
        01  Previous-Char           PIC X(1)  VALUE SPACES.
-       01  Balance                 PIC X(20) VALUE SPACES.
-     
-       
 
        PROCEDURE DIVISION.
        MOVE "Alexander" TO Fornavn
@@ -32,14 +19,14 @@
        MOVE "2500.75" TO Balance
        MOVE "DKK" TO Valutakode
 
-       STRING Fornavn DELIMITED BY SIZE " "
-              DELIMITED BY SIZE Efternavn
-              DELIMITED BY SIZE
+       STRING Fornavn DELIMITED BY SPACE
+              " "    DELIMITED BY SIZE
+              Efternavn DELIMITED BY SPACE
               INTO Navn
 
-       STRING Balance DELIMITED BY SPACE " "
-              DELIMITED BY SIZE Valutakode
-              DELIMITED BY SIZE
+       STRING Balance    DELIMITED BY SPACE
+              " "        DELIMITED BY SIZE
+              Valutakode DELIMITED BY SIZE
               INTO Balance-linje
 
        PERFORM VARYING IX FROM 1 BY 1
@@ -55,9 +42,11 @@
        MOVE Current-Char TO Previous-Char
 
        END-PERFORM
-       
+
       *Nedenfor kommer en display- Cobols måde at skrive i konsollen
-       DISPLAY KUNDEOPL
-       DISPLAY KONTOINFO
+       DISPLAY Kunde-id
+       DISPLAY Renset-Navn
+       DISPLAY Kontonummer
+       DISPLAY Balance-linje
 
        STOP RUN.
